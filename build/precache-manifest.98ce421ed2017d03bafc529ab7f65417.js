fd40f99fda6c05bd91bf02edbc8ec611aba1265b
HadiKhai <hadikhairallah@outlook.com>
2020-10-02 01:36:03 +0300

    fix: push build

---
+++ build/precache-manifest.98ce421ed2017d03bafc529ab7f65417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acaff9c999534eb6ab3a9554237e996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17cd1bd9cf63adba9240",
+    "url": "/static/css/main.ea33ae19.chunk.css"
+  },
+  {
+    "revision": "c48ee97df33ce64662c9",
+    "url": "/static/js/2.df35a2ce.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.df35a2ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17cd1bd9cf63adba9240",
+    "url": "/static/js/main.8da2945a.chunk.js"
+  },
+  {
+    "revision": "a4a011ee4cff22dbe791",
+    "url": "/static/js/runtime-main.d32ca624.js"
+  }
+]);
